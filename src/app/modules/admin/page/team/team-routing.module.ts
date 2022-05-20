fc35063b9c99e7c3c3d13e692e6a6814e2d456f6
Nikita <sominola@gmail.com>
2022-05-20 23:30:57 +0300

    fix after merge

--- src/app/modules/admin/page/team/team-routing.module.ts
+++ src/app/modules/admin/page/team/team-routing.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {TeamPageComponent} from "./team-page/team-page.component";
-import {SkillPageComponent} from "../skill/skill-page/skill-page.component";
-import {SkillDialogComponent} from "../skill/skill-dialog/skill-dialog.component";
 import {MatTableModule} from "@angular/material/table";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
 import {MatDialogModule} from "@angular/material/dialog";
-import {MaterialModule} from "../../../material/material.module";
 import {CommonModule} from "@angular/common";
 import {MatInputModule} from "@angular/material/input";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
@@ -32,7 +29,6 @@ const routes: Routes = [
       MatFormFieldModule,
       MatAutocompleteModule,
       MatDialogModule,
-      MaterialModule,
       CommonModule,
       MatInputModule,
       FormsModule,
