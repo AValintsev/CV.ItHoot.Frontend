f80f29f9f97b3c30d813779a80d0b5de73673871
Vitalii Ploshchynskyi <ploschinsskiyvv@gmail.com>
2022-06-01 11:45:58 +0300

    add shared/templates

--- src/app/modules/client/shared/header/header.component.ts
+++ src/app/modules/client/shared/header/header.component.ts
@@ -1,10 +1,10 @@
 import { SmallTeamDto } from './../../../../models/team/small-team-dto';
 import { ResumeService } from 'src/app/services/resume.service';
 import { Observable, of } from 'rxjs';
-import {Router} from '@angular/router';
-import {AccountService} from 'src/app/services/account.service';
-import {Component, OnInit} from '@angular/core';
-import {Users} from 'src/app/models/users-type';
+import { Router, ActivatedRoute, NavigationStart } from '@angular/router';
+import { AccountService } from 'src/app/services/account.service';
+import { Component, OnInit } from '@angular/core';
+import { Users } from 'src/app/models/users-type';
 import { map } from 'rxjs/operators';
 import { ClientTeamService } from 'src/app/services/client/client-team.service';
 
@@ -13,35 +13,70 @@ import { ClientTeamService } from 'src/app/services/client/client-team.service';
   selector: 'cv-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  
+
 })
 export class HeaderComponent implements OnInit {
   Users = Users
   teamList$!: Observable<SmallTeamDto[]>
-  userName$:Observable<string> = of('User')
+  userName$: Observable<string> = of('User')
   constructor(
     public accountService: AccountService,
     private router: Router,
-    private resumeService:ResumeService,
-    private clientTeamService: ClientTeamService
+    private resumeService: ResumeService,
+    private clientTeamService: ClientTeamService,
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-  this.teamList$ = this.clientTeamService.getAllTeam()
+    this.getTeamList()
     this.clientTeamService.getAllTeam().subscribe(console.log)
     this.userNamed()
-  }
-  
-  changeTeam(value:any){
-   console.log(value.source.value)
- 
-   this.clientTeamService.changeTeam(value.source.value)
+    console.log(this.router.url)
+    this.router.events.subscribe(e => console.log('events', e))
+    this.activatedRoute.url.subscribe(value => console.log('path parts: ', value));
   }
 
-  userNamed(){
-    if (this.accountService.getStoreRole() === Users[2]){
+  private getTeamList() {
+    this.teamList$ = this.clientTeamService.getAllTeam()
+  }
+
+  navigateToTeams() {
+this.router.navigate(['/client/teams/'])
+    this.router.events.subscribe({
+      next: response => {
+        if (response instanceof NavigationStart){
+          let regexp = new RegExp(`/client/teams`,'g')
+          const test = regexp.test(response.url);
+          console.log('test', test)
+          if(test){
+            
+            return true
+          }
+         
+        }
+        return []
+      },
+      error: error => { }
+    })
+
+    
+
+    
+
+    
+
+  }
+
+  changeTeam(value: any) {
+    console.log(value.source.value)
+
+    this.clientTeamService.changeTeam(value.source.value)
+  }
+
+  userNamed() {
+    if (this.accountService.getStoreRole() === Users[2]) {
       this.userName$ = this.resumeService.getAllResume().pipe(map(resume => resume[0]?.firstName))
-    } else if (!this.resumeService.getAllResume()){
+    } else if (!this.resumeService.getAllResume()) {
       this.userName$ = of('User')
     }
   }
