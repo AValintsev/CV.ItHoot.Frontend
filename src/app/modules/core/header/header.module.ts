36a4cdc4205f6f297d41de692403d099563734af
Vitalii Ploshchynskyi <ploschinsskiyvv@gmail.com>
2022-07-04 01:10:37 +0300

    add side bar transition

--- src/app/modules/core/header/header.module.ts
+++ src/app/modules/core/header/header.module.ts
@@ -1,12 +1,14 @@
+import { MatMenuModule } from '@angular/material/menu';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatButtonModule} from '@angular/material/button';
 import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './header.component';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-	imports:[CommonModule,MatButtonModule,RouterModule,MatTooltipModule],
+	imports:[CommonModule,MatButtonModule,RouterModule,MatTooltipModule,MatMenuModule,MatIconModule],
 	declarations: [HeaderComponent],
 	exports: [HeaderComponent]
 })
