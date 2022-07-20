f03438b5d35429945dc3f2cdbe203957c9601177
Vitalii Ploshchynskyi <vitalii.ploshchynskyi@ithoot.com>
2022-07-20 17:27:36 +0300

    fixing style left bar

--- src/app/modules/admin/admin-layout/admin-layout.component.ts
+++ src/app/modules/admin/admin-layout/admin-layout.component.ts
@@ -1,6 +1,4 @@
-import {AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
-import {MatSidenav} from "@angular/material/sidenav";
-import {BreakpointObserver} from "@angular/cdk/layout";
+import {Component, OnInit} from '@angular/core';
 
 
 @Component({
@@ -8,23 +6,15 @@ import {BreakpointObserver} from "@angular/cdk/layout";
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.scss']
 })
-export class AdminLayoutComponent implements AfterViewInit{
-  @ViewChild(MatSidenav)
-  sidenav!: MatSidenav;
-
-  constructor(private observer: BreakpointObserver,private cdr: ChangeDetectorRef) {}
-
-  ngAfterViewInit() {
-    this.observer.observe(['(max-width: 900px)']).subscribe((res) => {
-      if (res.matches) {
-        this.sidenav.mode = 'over';
-        this.sidenav.close();
-      } else {
-        this.sidenav.mode = 'side';
-        this.sidenav.open();
-      }
-    });
-    this.cdr.detectChanges()
+export class AdminLayoutComponent implements OnInit{
+  toggle = true
+  constructor() {
   }
 
+  ngOnInit(): void {
+
+  }
+  sideBarToggler(){
+    this.toggle = !this.toggle
+  }
 }
