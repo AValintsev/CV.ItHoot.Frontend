a0c0d2102d9d66e94364a6908f8d3ec3bd881527
Nikita <sominola@gmail.com>
2022-07-22 16:56:06 +0300

    change uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://159.69.35.199:8080/api/v1/'
+  apiUrl: 'https://cvbuilder-it.azurewebsites.net/api/v1/'
+  // apiUrl: 'http://159.69.35.199:8080/api/v1/'
 };
