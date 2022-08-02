dfd095a20af3c054b7438733aece46ef8ba3dec5
Nikita <sominola@gmail.com>
2022-08-02 13:20:54 +0300

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://cvbuilder-it.azurewebsites.net/api/v1/',
-  googleClientId: '1181654787190-3q6n92lmq06ktlqdgb32ejiv1h5ulnrr.apps.googleusercontent.com'
+  googleClientId: '181654787190-3q6n92lmq06ktlqdgb32ejiv1h5ulnrr.apps.googleusercontent.com'
   // apiUrl: 'http://159.69.35.199:8080/api/v1/'
 };
