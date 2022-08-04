c1b54c1bdbd7e41c70950c9cae4ff8551e450cb7
Vitalii Ploshchynskyi <vitalii.ploshchynskyi@ithoot.com>
2022-08-04 18:49:16 +0300

    rewrite menu

--- src/app/modules/admin/admin-layout/admin-layout.component.ts
+++ src/app/modules/admin/admin-layout/admin-layout.component.ts
@@ -1,21 +1,59 @@
-import {Component, OnInit} from '@angular/core';
-
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterContentInit,
+  AfterViewInit,
+} from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'cv-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss']
+  styleUrls: ['./admin-layout.component.scss'],
 })
-export class AdminLayoutComponent implements OnInit{
-  toggle = true
-  constructor(  ) {
-  }
+export class AdminLayoutComponent implements OnInit, AfterViewInit {
+  toggle = true;
+  @ViewChild('sideBar') sideBar: ElementRef<any>;
+  constructor() {}
 
-  ngOnInit(): void {
-
+  ngOnInit(): void {}
+  sideBarToggler(event:Event) {
+    event.stopPropagation()
+    this.toggle = !this.toggle;
   }
-  sideBarToggler(){
-    this.toggle = !this.toggle
-  }
+  ngAfterViewInit() {
+      // if (this.sideBar) {
+      //   fromEvent(document, 'click').subscribe({
+      //     next: (value) => {
 
+      //       if (window.innerWidth <= 768) {
+      //         let elem = value.target
+      //       if (
+      //         (value.target as HTMLDivElement).getAttribute('side-bar') !==
+      //         this.sideBar.nativeElement.getAttribute('side-bar')
+      //       ) {
+      //         if (this.toggle) {
+      //           this.toggle = !this.toggle;
+                
+      //           console.log((elem as HTMLElement).getAttribute('menu-item')==='menu-item')
+
+   
+      //         if((elem as HTMLElement).getAttribute('menu-item')=='menu-item'){
+      //           console.log((elem as HTMLElement).getAttribute('menu-item')==='menu-item')
+      //         this.toggle = true;
+      //         return;
+      //       }
+      //         }
+      //         return;
+      //       }
+      //     }
+        
+      //     },
+      //     error: (error) => console.log(error),
+      //   });
+      // }
+  
+  }
 }
