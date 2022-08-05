56b03765f0b1d5db9b5abe2ebb364484dacc6775
Vitalii Ploshchynskyi <ploschinsskiyvv@gmail.com>
2022-08-05 11:07:34 +0300

    ышву ифк

--- src/app/modules/admin/admin-layout/admin-layout.component.ts
+++ src/app/modules/admin/admin-layout/admin-layout.component.ts
@@ -24,36 +24,38 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit {
     this.toggle = !this.toggle;
   }
   ngAfterViewInit() {
-      // if (this.sideBar) {
-      //   fromEvent(document, 'click').subscribe({
-      //     next: (value) => {
+      if (this.sideBar) {
+        fromEvent(document, 'click').subscribe({
+          next: (value) => {
+            if (window.innerWidth <= 768) {
+              let elem = value.target
 
-      //       if (window.innerWidth <= 768) {
-      //         let elem = value.target
-      //       if (
-      //         (value.target as HTMLDivElement).getAttribute('side-bar') !==
-      //         this.sideBar.nativeElement.getAttribute('side-bar')
-      //       ) {
-      //         if (this.toggle) {
-      //           this.toggle = !this.toggle;
-                
-      //           console.log((elem as HTMLElement).getAttribute('menu-item')==='menu-item')
 
-   
-      //         if((elem as HTMLElement).getAttribute('menu-item')=='menu-item'){
-      //           console.log((elem as HTMLElement).getAttribute('menu-item')==='menu-item')
-      //         this.toggle = true;
-      //         return;
-      //       }
-      //         }
-      //         return;
-      //       }
-      //     }
+              if ((elem as HTMLElement).getAttribute('menu-item') == 'menu-item') {
+
+
+                this.toggle = true;
+                return;
+              }else{
+                 if (
+              (value.target as HTMLDivElement).getAttribute('side-bar') !==
+              this.sideBar.nativeElement.getAttribute('side-bar')
+            ) {
+              if (this.toggle) {
+                this.toggle = !this.toggle;        
+
+              }
+              return;
+            } 
+              }
+
+                        
+          }
         
-      //     },
-      //     error: (error) => console.log(error),
-      //   });
-      // }
+          },
+          error: (error) => console.log(error),
+        });
+      }
   
   }
 }
