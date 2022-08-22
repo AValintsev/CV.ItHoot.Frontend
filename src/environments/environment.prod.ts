e2412a47fcba0e84979fb27ea40691c122799fe5
sominola <sominola@gmail.com>
2022-08-22 23:43:04 +0300

    change uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cv.ithoot.com/api/api/v1/',
+  apiUrl: 'https://cvbuilder-it.azurewebsites.net/api/api/v1/',
   googleClientId: '181654787190-3q6n92lmq06ktlqdgb32ejiv1h5ulnrr.apps.googleusercontent.com',
 };
